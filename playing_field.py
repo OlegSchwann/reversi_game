472c1aff15f56f886d76fb432a19a3e39a7bab9c
Oleg <cup.of.software.code@gmail.com>
2017-07-14 13:05:00 +0300

    Поле реагирует на нажатия и ставит фишки только в возможных по правилам игры позициях, после чего происходит смена хода. Фишки пока не переварачиваются.

--- playing_field.py
+++ playing_field.py
@@ -1,5 +1,6 @@
 import pygame
 
+# стоит отметить, что далее оси: y→ , x↓.
 class PlayingField:
     def __init__(self):
         # игровое поле 0-пусто, 1-белый, 2-чёрный
@@ -15,19 +16,29 @@ class PlayingField:
             [0, 0, 0, 0, 2, 0, 1, 1, 1, 0],
             [0, 0, 0, 1, 1, 1, 0, 0, 0, 0]]
         # чей ход. чёрные ходят первыми.
-        self.stroke = 1
+        self.stroke = 2
+        # размеры игрового поля в пикселях
+        self.width = 0
+        self.height = 0
 
     def find_moves(self):
-        # функция шерстит поле, и отмечает на нём цифрой 3 те места, в которые текущий игрок может сделать ход
+        # функция шерстит поле, и отмечает на нём те места, в которые текущий игрок может сделать ход
         # обнуляем оставшееся с прошлого хода
         for x in range(0, self.field.__len__()):
             for y in range(0, self.field[x].__len__()):
-                if self.field[x][y] == 3:
+                if self.field[x][y] not in {0, 1, 2}:
                     self.field[x][y] = 0
         # (самое сложное для компьютера)
         # теперь рассматриваем каждую существующую фишку ходящего игрока,
-        # идём от неё во все 8 сторон, и если оказывается, что там только фишки противоположного игрока и потом пустота,
-        # то отмечаем тройкой
+        # идём от неё во все 8 сторон,
+        #   записывая в список кординаты фишек,
+        # если оказывается,
+        # что ((сначала идут в ряд только фишки противоположного игрока) и
+        #      (потом пустота или отметка о другом возможном ходе)),
+        # то этот список с фишками, которые перевернутся при ходе
+        # помещаем в клетку с отметкой пусто или
+        # сливаем с уже существующим списком, оставшимся от другого хода
+        # длина списка будет суммарным выигрышем за этот ход
         Y_LEN = self.field[0].__len__()
         X_LEN = self.field.__len__()
         for x in range(0, self.field.__len__()):
@@ -38,7 +49,7 @@ class PlayingField:
                     while step_y >= 0:
                         if self.field[x][step_y] == self.stroke: # если своя фишка загораживает
                             break
-                        if self.field[x][step_y] == 0: # если пустое место
+                        if self.field[x][step_y] not in {1, 2}: # если (пустое место или возможен дугой ход) == (не стоит никакая фишка)
                             if y-step_y > 1: # и не соседняя клетка с исходной
                                 self.field[x][step_y] = 3  # отмечаем можно сходить
                             break
@@ -49,7 +60,7 @@ class PlayingField:
                     while step_y >= 0 and step_x >= 0:
                             if self.field[step_x][step_y] == self.stroke:  # если своя фишка загораживает
                                 break
-                            if self.field[step_x][step_y] == 0:  # если пустое место
+                            if self.field[step_x][step_y] not in {1, 2}: # если не стоит никакая фишка
                                 if y - step_y > 1:  # и не соседняя клетка с исходной
                                     self.field[step_x][step_y] = 3  # отмечаем можно сходить
                                 break
@@ -60,7 +71,7 @@ class PlayingField:
                     while step_x >= 0:
                         if self.field[step_x][y] == self.stroke:  # если своя фишка загораживает
                             break
-                        if self.field[step_x][y] == 0:  # если пустое место
+                        if self.field[step_x][y] not in {1, 2}: # если не стоит никакая фишка
                             if x - step_x > 1:  # и не соседняя клетка с исходной
                                 self.field[step_x][y] = 3  # отмечаем можно сходить
                             break
@@ -71,7 +82,7 @@ class PlayingField:
                     while step_y < Y_LEN and step_x >= 0:
                         if self.field[step_x][step_y] == self.stroke:  # если своя фишка загораживает
                             break
-                        if self.field[step_x][step_y] == 0:  # если пустое место
+                        if self.field[step_x][step_y] not in {1, 2}: # если не стоит никакая фишка
                             if step_y - y > 1:  # и не соседняя клетка с исходной
                                 self.field[step_x][step_y] = 3  # отмечаем можно сходить
                             break
@@ -82,7 +93,7 @@ class PlayingField:
                     while step_y < Y_LEN:
                         if self.field[x][step_y] == self.stroke:  # если своя фишка загораживает
                             break
-                        if self.field[x][step_y] == 0:  # если пустое место
+                        if self.field[x][step_y] not in {1, 2}: # если не стоит никакая фишка
                             if step_y - y > 1:  # и не соседняя клетка с исходной
                                 self.field[x][step_y] = 3  # отмечаем можно сходить
                             break
@@ -93,7 +104,7 @@ class PlayingField:
                     while step_y < Y_LEN and step_x < X_LEN:
                         if self.field[step_x][step_y] == self.stroke:  # если своя фишка загораживает
                             break
-                        if self.field[step_x][step_y] == 0:  # если пустое место
+                        if self.field[step_x][step_y] not in {1, 2}: # если не стоит никакая фишка
                             if step_y - y > 1:  # и не соседняя клетка с исходной
                                 self.field[step_x][step_y] = 3  # отмечаем можно сходить
                             break
@@ -104,7 +115,7 @@ class PlayingField:
                     while step_y < Y_LEN and step_x < X_LEN:
                         if self.field[step_x][y] == self.stroke:  # если своя фишка загораживает
                             break
-                        if self.field[step_x][y] == 0:  # если пустое место
+                        if self.field[step_x][y] not in {1, 2}: # если не стоит никакая фишка
                             if step_x - x > 1:  # и не соседняя клетка с исходной
                                 self.field[step_x][y] = 3  # отмечаем можно сходить
                             break
@@ -115,7 +126,7 @@ class PlayingField:
                     while step_y >= 0 and step_x < X_LEN:
                         if self.field[step_x][step_y] == self.stroke:  # если своя фишка загораживает
                             break
-                        if self.field[step_x][step_y] == 0:  # если пустое место
+                        if self.field[step_x][step_y] not in {1, 2}: # если не стоит никакая фишка
                             if y - step_y > 1:  # и не соседняя клетка с исходной
                                 self.field[step_x][step_y] = 3  # отмечаем 'можно сходить'
                             break
@@ -127,44 +138,58 @@ class PlayingField:
         # меняет игрока
         # № строки задаётся с 0 сверху
         # № столбца слева на право
-        if self.field[row][column] == 3:  # ход возможен
+        print("пытаемся поставить {player} фишку в строчке {row}, столбце {column}".format(player="белую" if self.stroke == 1 else "чёрную", row=row, column=column))
+        if self.field[row][column] not in {0, 1, 2}:  # ход возможен
             self.field[row][column] = self.stroke
             self.stroke = 1 if self.stroke == 2 else 2 # инвертирует игрока
+            # переразмечает поле возможными ходами для следующего игрока
+            self.find_moves()
         # ничего не происходит при неправильном запросе
 
     def draw_field(self, screen):
-        width = screen.get_width()
-        height = screen.get_height()
+        self.width = screen.get_width()
+        self.height = screen.get_height()
         # заполняем поле жизнерадостным зелёным
         screen.fill((50, 205, 50))  # limegreen
         # рисуем вертикальные линии
-        for i in range(width // 10, width, width // 10):
-            pygame.draw.line(screen, (51, 102, 51), (i, 0), (i, height), 3)
+        for i in range(self.width // 10, self.width, self.width // 10):
+            pygame.draw.line(screen, (51, 102, 51), (i, 0), (i, self.height), 3)
         # горизонтальные
-        for i in range(height // 10, height, height // 10):
-            pygame.draw.line(screen, (51, 102, 51), (0, i), (width, i), 3)
+        for i in range(self.height // 10, self.height, self.height // 10):
+            pygame.draw.line(screen, (51, 102, 51), (0, i), (self.width, i), 3)
         # расставляем белые чёрные и маленькие жёлтые круги
         for x in range(0, self.field.__len__()):
             for y in range(0, self.field[x].__len__()):
                 # белая фишка
                 if self.field[x][y] == 1:
-                    pygame.draw.circle(screen, (204, 255, 204), self.cell_center(x, y), width//30, 10)
+                    pygame.draw.circle(screen, (243, 255, 239), self.cell_center(x, y), self.width//30, 0)
+                    pygame.draw.circle(screen, (177, 198, 171), self.cell_center(x, y), self.width // 30, 5)
                 # чёрная фишка
                 if self.field[x][y] == 2:
-                    pygame.draw.circle(screen, (0, 55, 0), self.cell_center(x, y), width // 30, 10)
+                    pygame.draw.circle(screen, (0, 55, 0), self.cell_center(x, y), self.width // 30, 0)
+                    pygame.draw.circle(screen, (23, 99, 0), self.cell_center(x, y), self.width // 30, 5)
                 # точка возможности хода
-                if self.field[x][y] == 3:
-                    pygame.draw.circle(screen, (204, 255, 153), self.cell_center(x, y), width // 50, 10)
+                if self.field[x][y] not in {0, 1, 2}:
+                    pygame.draw.circle(screen, (61, 109, 46), self.cell_center(x, y), self.width // 60, 2)
 
-    def cell_center(self, x, y, width = 1000, height = 1000):
+    def cell_center(self, x, y):
         # возвращает абсолютные координаты центра клетки
         # по её кордитанам x, y
-        return width//20 * (2*y + 1), height//20 * (2*x + 1)
+        return self.width//20 * (2*y + 1), self.height//20 * (2*x + 1)
 
+    def process_click(self, y, x):
+        # принимает абсолютные координаты нажатия (y→, x↓) и если они во внутренней области(80% ширины клетки)
+        # то пытается сделать ход по относительным координатам
+        if (self.width//100  < y%(self.width//10)  <  self.width//100*9) and \
+           (self.height//100 < x%(self.height//10) < self.height//100*9):
+            self.move(x // (self.height // 10),  # x↓
+                      y // (self.width // 10))   # y→
 
-# test area*
+    def оutput_to_console(self):
+        for line in self.field:
+            print(line)
 
-play_field = PlayingField()
-play_field.find_moves()
-for line in play_field.field:
-    print(line)
+    def flip_chips(self):
+        # алгоритм переворота фишек соперника при удачном ходе
+        # использует список кортежей(x, y) который хранится в ячейках, возможных для хода.
+        pass
--- reversy.py
+++ reversy.py
@@ -7,11 +7,11 @@ field_side = 1000
 screen = pygame.display.set_mode((field_side, field_side), 0, 32)
 pygame.display.set_caption("Реверси")
 
-
-bgColor = (50, 205,	50)  # limegreen
-
-# initial data here
+# инициализируем поле для игры
 playing_field = playing_field.PlayingField()
+# и рисуем его
+playing_field.find_moves()
+playing_field.draw_field(screen)
 
 mainLoop = True
 while mainLoop:
@@ -19,9 +19,10 @@ while mainLoop:
         if event.type == QUIT:
             mainLoop = False
         if event.type == MOUSEBUTTONDOWN:
-            # create frame here
-
+            # передаём полю абсолютные координаты нажатия
+            playing_field.process_click(*event.pos)
+            # создаём картинку только после изменения поля, что б не грузить процессор на 100%
+            # playing_field.оutput_to_console()
             playing_field.draw_field(screen)
-
     pygame.display.update()
 pygame.quit()
